453a287fdb7b62eeaf8a9b3dd2c23f104cfe5451
haitan2408 <haitan28102408@gmail.com>
2024-11-02 21:11:20 +0700

    up demo ss1

--- src/ss1_introduction_to_java/Demo.java
+++ src/ss1_introduction_to_java/Demo.java
@@ -4,6 +4,17 @@ package ss1_introduction_to_java;
 public class Demo {
 
     public static void main(String[] args) {
-
+        final byte a = 127;
+        long b = 5;
+        long c = 23497893473578934L;
+        long d = 5;
+        float e = 2347939757349573L;
+        double g = 34234242.0;
+        boolean x = 5 == 5;
+        System.out.println();
+        // cơ chế ép kiểu ngầm định của kiểu nguyên thủy (implicit casting)
+//    switch (12423423L) {
+//
+//    }
     }
  }
