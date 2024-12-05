32a5fbdce840c078c3574b70c7e902f7fd3ba7b3
haitan2408 <haitan28102408@gmail.com>
2024-12-05 21:19:23 +0700

    up demo ss14

---
+++ src/ss14_sort/DemoSort.java
@@ -0,0 +1,67 @@
+package ss14_sort;
+
+public class DemoSort {
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+    }
+
+    public static void bubbleSort2(int[] arr) {
+        for(int i =0; i < arr.length - 1; i++) {
+            for(int j =0; j< arr.length - i - 1; j++) {
+                if(arr[j] < arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+ 1];
+                    arr[j+ 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
